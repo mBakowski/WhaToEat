8de111be12d2992c179de6a638b71f661b90545a
Michał Bąkowski <micbakow92@gmail.com>
2019-08-09 01:58:24 +0200

    Preparing user communication

---
+++ Logic/Client.py
@@ -0,0 +1,24 @@
+class Client:
+
+    def userIngredientAsk(self):
+
+        ingredient = input("Wprowadź posiadany składnik: ")
+        while True:
+            if ingredient.isdigit():
+                ingredient = input("Proszę o podanie nazwy składnika: ")
+            else:
+                return ingredient
+
+    def userMenuDisplay(self):
+        print("1) Wyszukiwanie na podstawie składnika")
+        print("2) Wyświetl wszystkie przepisy")
+        print("3) Wyświetl wszystkie składniki")
+        print("4) Dodawanie nowego przepisu")
+        print("5) Dodawanie nowego składnika")
+        print("0) Wyjście z programu")
+
+    def userPrepMessage(self):
+        print("Ups, funkcjonalność w przygotowaniu")
+
+    def userErrorMessage(self):
+        print("UPS, COŚ POSZŁO NIE TAK")
--- Main/Main.py
+++ Main/Main.py
@@ -1,3 +1,6 @@
 from DataBase.DataStorage import DataStorage
+from Logic.Client import Client
 
+test2 = Client()
 test = DataStorage()
+
